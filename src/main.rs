b8e1d22efdf4a4885e02a446ae15fd38d2284406
Laurent Chindeko <laurent.chindeko.hk@gmail.com>
2023-01-16 12:27:01 +0800

    added specific code for wasm32-wasi

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // We'll bind to 127.0.0.1:3000
     let addr = ([127,0,0,1], 3000).into();
 
-    // A 'Service' is needed for ever connection, so this
-    // creates one from our 'hello_world' function.
-    let make_svc = make_service_fn(|_conn| {
-        // service_fn converts our function into a 'Service'
-        async { Ok::<_, Infallible>(service_fn(hello_world)) }
-    });
+    #[cfg(target_arch = "wasm32")]
+    {
+        use tokio::net::TcpListener;
+        use hyper::server::conn::Http;
 
-    let server = Server::bind(&addr).serve(make_svc);
+        let listener = TcpListener::bind(addr).await?;
+        println!("Listening on http://{}", addr);
+        loop {
+            let (stream, _) = listener.accept().await?;
 
-    // Run this server for... forever!
-    // if let Err(e) = server.await {
-    //     eprintln!("Server error: {}", e);
-    // }
-    println!("Listening on http://{}", addr);
+            tokio::task::spawn(async move {
+                if let Err(err) = Http::new().server_connection(stream, service_fn(hello_world)).await {
+                    println!("Error serving connection: {:?}", err);
+                }
+            });
+        }
+    }
 
-    server.await?;
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        // A 'Service' is needed for ever connection, so this
+        // creates one from our 'hello_world' function.
+        let make_svc = make_service_fn(|_conn| {
+            // service_fn converts our function into a 'Service'
+            async { Ok::<_, Infallible>(service_fn(hello_world)) }
+        });
 
-    Ok(())
+        let server = Server::bind(&addr).serve(make_svc);
+
+        // Run this server for... forever!
+        // if let Err(e) = server.await {
+        //     eprintln!("Server error: {}", e);
+        // }
+        println!("Listening on http://{}", addr);
+
+        server.await?;
+
+        Ok(())
+    }
 }
